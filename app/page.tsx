5bb579f7f03cddb9de79c50a299d47e494a0889b
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-10 19:23:30 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: 50, textAlign: "center" }}>
-      <h1>Welcome to LearnLynk</h1>
-      <p>Go to <a href="/dashboard/today">Dashboard</a></p>
-    </div>
-  );
-}
