b5d6fbe71d66231ed3e68869359494fcc6056ac2
AsthaPitambarwale666 <167300135+AsthaPitambarwale@users.noreply.github.com>
2025-12-10 19:13:51 +0530

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to LearnLynk!</h1>
+    <div style={{ padding: 50, textAlign: "center" }}>
+      <h1>Welcome to LearnLynk</h1>
+      <p>Go to <a href="/dashboard/today">Dashboard</a></p>
     </div>
   );
 }
